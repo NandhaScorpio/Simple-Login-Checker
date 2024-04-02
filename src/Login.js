aa8e37e122ccac6f35faf0699c333060cc2c5128
NandhaScorpio <gokulamstore2010@gmail.com>
2024-04-02 11:32:57 +0530

    Updated a simple login page using react

---
+++ src/Login.js
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+function Login() {
+  const id = "Nandha";
+  const password = "123";
+
+  const [loginid, setLoginId] = useState("");
+  const [loginPassword, setPassword] = useState("");
+
+  const [loginput, setLogInput] = useState(true);
+  const [loginPage, setLoginPage] = useState(false);
+
+  const nid = 0;
+
+  console.log(nid);
+
+  function handleusername(evt) {
+    setLoginId(evt.target.value);
+  }
+
+  function handlepassword(evt) {
+    setPassword(evt.target.value);
+  }
+
+  function idchecking() {
+    const userCheck = id === loginid;
+    const passCheck = password === loginPassword;
+
+    const nid = userCheck === true && passCheck === true;
+
+    setLogInput(!nid);
+    setLoginPage(nid);
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <h1 style={{ display: loginPage ? "block" : "none" }}>Hello Nandha</h1>
+      <label style={{ display: loginput ? "block" : "none" }}>Username :</label>
+      <input
+        type="text"
+        placeholder="Enter your Username..."
+        style={{ display: loginput ? "block" : "none" }}
+        onChange={handleusername}
+        value={loginid}
+      />
+      <br />
+      <label style={{ display: loginput ? "block" : "none" }}>
+        Password :{" "}
+      </label>
+      <input
+        type="text"
+        placeholder="Enter your password..."
+        value={loginPassword}
+        onChange={handlepassword}
+        style={{ display: loginput ? "block" : "none" }}
+      />{" "}
+      <br />
+      <button
+        style={{ display: loginput ? "block" : "none" }}
+        onClick={idchecking}
+      >
+        Login
+      </button>
+
+      <h1 style={{ display: loginput ? "block" : "none" }}>The username is : Nandha</h1>
+      <h1 style={{ display: loginput ? "block" : "none" }}>The Password is : 123</h1>
+    </div>
+  );
+}
+
+export default Login;
